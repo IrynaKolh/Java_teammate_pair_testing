f81e44650256e01a0334b580c1fde7107a4607a5
Lokankara <poliyakpavlo@gmail.com>
2024-03-05 22:27:11 +0200

    Added test-cases for find median

--- src/test/java/MedianTest.java
+++ src/test/java/MedianTest.java
@@ -1,8 +1,44 @@
-import org.junit.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class MedianTest {
+
+    private Median median;
+    @BeforeClass
+    public void beforeClass() {
+        median = new Median();
+    }
+
     @Test
     public void testMedianHN() {
 
+    }
+
+    @Test(dataProvider = "validNumberArrayProvider",
+            dataProviderClass = DataNumbersProvider.class)
+    public void testFindMedianValidNumberArrayProvider(int[] input, double expected) {
+        double actual = median.median(input);
+        assertEquals(expected, actual);
+    }
+    @Test(dataProvider = "validNumberArrayProvider",
+            dataProviderClass = DataNumbersProvider.class)
+    public void testFindMedianValidNumberArrayProviderQuickSelect(int[] input, double expected) {
+        double actual = median.median(input);
+        assertEquals(expected, actual);
+    }
+    @Test(dataProvider = "invalidNumberArrayProvider",
+            dataProviderClass = DataNumbersProvider.class)
+    public void testCalculateMedianWithInvalidInput(int[] numbers, double expected) {
+        double actual = median.median(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test(dataProvider = "singleElementArrayProvider",
+            dataProviderClass = DataNumbersProvider.class)
+    public void testFindMedianValidNumberArrayProviderWithSingleElementArrayProvider(int[] input, double expected) {
+        double actual = median.median(input);
+        assertEquals(actual, expected);
     }
 }
