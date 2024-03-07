d5295ddcc4322e726dc892e5c3de6ca7e0a6b5bf
Lokankara <poliyakpavlo@gmail.com>
2024-03-07 11:36:39 +0200

    Fixed sized array, when incorrect numbers of day Added cases test for months

--- src/test/java/time/MonthDaysCalculatorTest.java
+++ src/test/java/time/MonthDaysCalculatorTest.java
@@ -3,7 +3,8 @@ package time;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
 
 public class MonthDaysCalculatorTest {
 
@@ -12,5 +13,19 @@ public class MonthDaysCalculatorTest {
         String[] actual = MonthDaysCalculator.getMonthsByDays(days);
 
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test(dataProvider = "enumDaysProvider", dataProviderClass = MonthProvider.class)
+    public void testGetMonthsByDays(int days, String[] expected) {
+        String[] result = MonthDaysCalculator.getMonthsByDays(days);
+
+        assertArrayEquals(result, expected);
+    }
+
+    @Test(dataProvider = "invalidDaysProvider", dataProviderClass = MonthProvider.class)
+    public void testGetMonthsByInvalidDays(int days) {
+        String[] result = MonthDaysCalculator.getMonthsByDays(days);
+
+        assertEquals(result.length, 0);
     }
 }
--- src/test/java/time/MonthProvider.java
+++ src/test/java/time/MonthProvider.java
@@ -2,6 +2,8 @@ package time;
 
 import org.testng.annotations.DataProvider;
 
+import static time.MonthType.*;
+
 public class MonthProvider {
     @DataProvider(name = "validDays")
     public Object[][] daysProvider() {
@@ -12,10 +14,39 @@ public class MonthProvider {
                 {31, new String[]{"January", "March", "May", "July", "August", "October", "December" }},
                 {-1, new String[]{}},
                 {32, new String[]{}},
-//                {27, new String[]{}},
-//                {1, new String[]{}},
-//                {0, new String[]{}},
-//                {null, new String[]{}},
+                {27, new String[]{}},
+                {1, new String[]{}},
+                {0, new String[]{}},
+                {Integer.MIN_VALUE, new String[]{}},
+                {Integer.MAX_VALUE, new String[]{}}
+        };
+    }
+
+    @DataProvider(name = "enumDaysProvider")
+    public Object[][] enumDaysProvider() {
+        return new Object[][] {
+                {31, new String[] {
+                        JANUARY.getName(), MARCH.getName(),
+                        MAY.getName(), JULY.getName(),
+                        AUGUST.getName(), OCTOBER.getName(),
+                        DECEMBER.getName()}},
+                {30, new String[] {
+                        APRIL.getName(), JUNE.getName(),
+                        SEPTEMBER.getName(), NOVEMBER.getName()}},
+                {29, new String[] {FEBRUARY.getName()}},
+                {28, new String[] {FEBRUARY.getName()}}
+        };
+    }
+
+    @DataProvider(name = "invalidDaysProvider")
+    public Object[][] invalidDaysProvider() {
+        return new Object[][] {
+                {-1},
+                {0},
+                {1},
+                {27},
+                {32},
+                {100}
         };
     }
 }
