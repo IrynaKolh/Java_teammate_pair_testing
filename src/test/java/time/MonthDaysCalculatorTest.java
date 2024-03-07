c7c58c338f2dbf6595712fbd050d400aa7e3e9cb
Iryna Kolhanova <96072009+IrynaKolh@users.noreply.github.com>
2024-03-07 04:16:59 -0500

    Month test (#5)
    * add median method
    * fix: median function
    * fix: null err
    * add my test
    * feat: add MonthDaysCalculatorTest
    * fix: delete .idea
    * fix: comment err

--- src/test/java/time/MonthDaysCalculatorTest.java
+++ src/test/java/time/MonthDaysCalculatorTest.java
@@ -1,11 +1,16 @@
 package time;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
 
 public class MonthDaysCalculatorTest {
 
     @Test(dataProvider = "validDays", dataProviderClass = MonthProvider.class)
     public void testName(int days, String[] expected) {
+        String[] actual = MonthDaysCalculator.getMonthsByDays(days);
 
+        Assert.assertEquals(expected, actual);
     }
 }
--- src/test/java/time/MonthProvider.java
+++ src/test/java/time/MonthProvider.java
@@ -5,6 +5,17 @@ import org.testng.annotations.DataProvider;
 public class MonthProvider {
     @DataProvider(name = "validDays")
     public Object[][] daysProvider() {
-        return new Object[][]{};
+        return new Object[][]{
+                {28, new String[]{"February"}},
+                {29, new String[]{"February"}},
+                {30, new String[]{"April", "June", "September", "November" }},
+                {31, new String[]{"January", "March", "May", "July", "August", "October", "December" }},
+                {-1, new String[]{}},
+                {32, new String[]{}},
+//                {27, new String[]{}},
+//                {1, new String[]{}},
+//                {0, new String[]{}},
+//                {null, new String[]{}},
+        };
     }
 }
